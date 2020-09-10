c7be29ce54adee7be266aba4abb2bb523ba47098
Rafael Mendes <mendesrafa.c@gmail.com>
2020-09-10 00:46:12 -0300

    attempting to unflip cards when they don't match

--- assets/js/cardFlip.js
+++ assets/js/cardFlip.js
@@ -6,3 +6,40 @@ getCards.forEach(function (card) {
         card.querySelector(".back-face").classList.add("rotate-back-face");
     }
 )});
+
+/*let hasFlipped = false;
+let firstCard, secondCard;
+
+function flippedCondition () {
+    
+    this.querySelector(".front-face").classList.add("rotate-front-face");
+    this.querySelector(".back-face").classList.add("rotate-back-face");
+        
+    if (!hasFlipped) {
+        hasFlipped = true;
+        firstCard = this;
+        var firstAlt = firstCard.querySelector(".back-face img").alt;
+    } else {
+        hasFlipped = false;
+        secondCard = this;
+        var secondAlt = secondCard.querySelector(".back-face img").alt;
+    }
+    
+    if (firstAlt === secondAlt) {
+        firstCard.removeEventListener('click', flippedCondition);
+        secondCard.removeEventListener('click', flippedCondition);
+    } else {
+        setTimeout (function (){
+            /*unflipCard(previousCard);
+            unflipCard(currentCard);
+            firstCard.querySelector(".front-face").classList.remove("rotate-front-face");
+            firstCard.querySelector(".back-face").classList.remove("rotate-back-face");
+
+            secondCard.querySelector(".front-face").classList.remove("rotate-front-face");
+            secondCard.querySelector(".back-face").classList.remove("rotate-back-face");
+
+        }, 1000);
+    }
+};
+
+getCards.forEach(card => card.addEventListener('click', flippedCondition));*/
