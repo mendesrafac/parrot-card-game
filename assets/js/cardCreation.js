49940c3936c065ea6b993f278490488af279199a
Rafael Mendes <mendesrafa.c@gmail.com>
2020-09-09 21:21:37 -0300

    cardCreation working as intended

--- assets/js/cardCreation.js
+++ assets/js/cardCreation.js
@@ -1,4 +1,5 @@
-const imgPaths = [
+/* the array imgPaths contains all paths to back images of the project */
+var imgPaths = [
     '<img src="./assets/images/bobrossparrot.gif" alt="bobrossparrot">',
     '<img src="./assets/images/explodyparrot.gif" alt="explodyparrot">',
     '<img src="./assets/images/fiestaparrot.gif" alt="fiestaparrot">',
@@ -8,24 +9,33 @@ const imgPaths = [
     '<img src="./assets/images/unicornparrot.gif" alt="unicornparrot">',
 ];
 
-function createCards () {
+function createCard () {
     var card = document.createElement("div");
     var front = document.createElement("div");
     var back = document.createElement("div");
-    
+        
     card.classList.add("card");
     front.classList.add("face");
     front.classList.add("front-face");
     back.classList.add("face");
     back.classList.add("back-face");
-
+    
     card.appendChild(front);
     card.appendChild(back);
     front.innerHTML = "<img src='./assets/images/front.png' alt='parrot'>";   
-    
-    var backImg = imgPaths[Math.floor(Math.random()*imgPaths.length)];
-
+    /* Gets random position of imgPaths */    
+    var randomPos = Math.floor(Math.random()*imgPaths.length);
+        
+    var backImg = imgPaths[randomPos];
+    /* add back image to the card */    
     back.innerHTML = backImg;
-    
+
+    /*  removes the back image path from the imgPaths array
+        so it can't be selected more than once */
+    imgPaths = imgPaths.filter(item => item != backImg);
+
     return card;
 }
+
+
+
