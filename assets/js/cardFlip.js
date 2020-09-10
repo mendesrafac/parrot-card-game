f47dbdb99da1a66260ab3415df767eee5a2a3226
Rafael Mendes <mendesrafa.c@gmail.com>
2020-09-09 21:38:46 -0300

    wip: cardFlip only flips when clicked

--- assets/js/cardFlip.js
+++ assets/js/cardFlip.js
@@ -1,4 +1,6 @@
-cards.forEach(function (card) {
+var getCards = Array.from(document.querySelectorAll(".card"));
+
+getCards.forEach(function (card) {
     card.addEventListener('click', function() {
         card.querySelector(".front-face").classList.add("rotate-front-face");
         card.querySelector(".back-face").classList.add("rotate-back-face");
