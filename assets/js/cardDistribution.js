280cdda249f5dbc806d9c61430c34452c1905bea
Rafael Mendes <mendesrafa.c@gmail.com>
2020-09-09 21:30:48 -0300

    cardDistribution working as intended

--- assets/js/cardDistribution.js
+++ assets/js/cardDistribution.js
@@ -1,12 +1,32 @@
-var cards = Array.from(document.querySelectorAll(".card"));
+/* gets the number of cards the player wants in the game */
 var numCards = parseInt(prompt("Quantas cartas quer jogar? Escolha um número entre 4 e 14"));
 
+/* while numCards < 4, > 14, an odd number or not a number at all (treats strings) */
 while(numCards < 4 || numCards > 14 || numCards & 1 || isNaN(numCards)) {
     numCards = parseInt(prompt("Quantas cartas quer jogar? Escolha um número entre 4 a 14"));
 }
 
-let shuffled = cards.sort(() => .5 - Math.random()).slice(0,numCards);
+/*  need to work with pairs as it's necessary that
+    cards are duplicated */
+var numPairs = numCards / 2;
 
-shuffled.forEach(function(element) {
-    element.classList.add("visible");
+/* the array cards starts empty */
+var cards = [];
+
+/*  and it's filled everytime a card is created by
+    the card itself and it's clone */
+for (let index = 0; index < numPairs; index++) {
+    var card = createCard();
+    var clone = card.cloneNode(true);
+    cards.push(card);
+    cards.push(clone);
+}
+
+/*  here, a non optimal, yet simple shuffle method is used
+    because the cards.length will always be small */
+var shuffledCards = cards.sort(() => .5 - Math.random());
+
+/* with the cards shuffled, appendChild is used */
+shuffledCards.forEach(function (card) {
+    document.querySelector("main").appendChild(card);
 });
